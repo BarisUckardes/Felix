016bb2c43ade7df5d1086a98bc47f22369412666
Barış Üçkardeş <68399075+BarisUckardes@users.noreply.github.com>
2022-10-17 19:46:31 +0300

    Pipeline

--- Felix/src/Graphics/Pipeline/PipelineCreateDesc.h
+++ Felix/src/Graphics/Pipeline/PipelineCreateDesc.h
@@ -1,3 +1,26 @@
 #pragma once
 #include <Graphics/Pipeline/DepthStencilStateDesc.h>
 #include <Graphics/Pipeline/RasterizerStateDesc.h>
+#include <Graphics/Pipeline/BlendingStateCreateDesc.h>
+#include <Graphics/Pipeline/ResourceStateDesc.h>
+#include <Graphics/Pipeline/ShaderPassDesc.h>
+#include <Graphics/Pipeline/VertexLayoutDesc.h>
+#include <Graphics/Pipeline/OutputDesc.h>
+#include <Graphics/Pipeline/PipelineType.h>
+
+namespace Felix
+{
+	/// <summary>
+	/// Required information to create a pipeline
+	/// </summary>
+	struct EXPORT PipelineCreateDesc
+	{
+		RasterizerStateDesc RasterizerDesc;
+		DepthStencilStateDesc DepthStencilDesc;
+		BlendingStateCreateDesc BlendingDesc;
+		VertexLayoutDesc InputLayoutDesc;
+		ShadingPassDesc ShadingPass;
+		OutputDesc OutputDesc;
+		PipelineType Type;
+	};
+}
